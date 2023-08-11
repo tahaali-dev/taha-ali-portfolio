da0dd6eea1a011fa0e5cbfa847a082ebb65d975f
devtahajs <tahaali.css@gmail.com>
2023-08-11 23:00:54 +0530

    some adds

---
+++ src/Components/TechSect/Tech2.jsx
@@ -0,0 +1,49 @@
+import "./Tech2.css";
+const Tech2 = () => {
+  return (
+    <div className="width-100">
+      <div className="width-85 tech2-cont">
+        <div className="tec2-left">
+          <div className="icon-cont">
+            <div className="icon-w ">
+              <img className="node" src="/logos/nodejs.png" alt="node" />
+              <p>NodeJs</p>
+            </div>
+
+            <div className="icon-w">
+              <img className="express" src="/logos/express.png" alt="express" />
+              <p>ExpressJs</p>
+            </div>
+
+            <div className="icon-w">
+              <img className="mongo" src="/logos/mongo.png" alt="mongo" />
+              <p>MongoDB</p>
+            </div>
+
+            <div className="icon-w">
+              <img className="sql" src="/logos/sql.png" alt="sql" />
+              <p>MySql</p>
+            </div>
+
+            <div className="icon-w">
+              <img className="jwt" src="/logos/jwt.png" alt="jwt" />
+              <p>JWt</p>
+            </div>
+
+            <div className="icon-w">
+              <img className="prisma" src="/logos/prisma.png" alt="prisma" />
+              <p>Prisma</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="tec2-right">
+          <h2>Server</h2>
+          <h3>Side</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tech2;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Hero from "../../Components/HeroSec/Hero";
 import About from "../../Components/AboutSection/About";
 import TechSec from "../../Components/TechSect/TechSec";
-
+import Tech2 from "../../Components/TechSect/Tech2";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <TechSec />
+      <Tech2 />
       <About />
     </>
   );
